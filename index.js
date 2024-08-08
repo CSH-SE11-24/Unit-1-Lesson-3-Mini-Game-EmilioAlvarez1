d23590e350ed79725f983389fd8c9126f55cf364
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-08 10:26:43 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,21 @@
+// Get started!
+
+
+const name = prompt("insert your username")
+console.log(name + ", Welcome to would you rather")
+
+let One = prompt("GMC or Cadillac")
+console.log(name + ", You picked, " + One)
+
+let two = prompt("Intelligent or Smart")
+console.log(name + ", You selected, " + two)
+
+let three = prompt("Math or Physics")
+console.log(name + ", You picked, " + three)
+
+let four = prompt("Hot or Cold")
+console.log(name + ", You selected, " + four)
+
+
+
+console.log("you went to level " + number("2"))
